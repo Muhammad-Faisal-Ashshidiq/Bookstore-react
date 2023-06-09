0ec5d7e231a0beae7f5e7d24767b251604db90de
Muhammad Faisal A <sidiqfaisal30@gmail.com>
2023-06-09 18:51:05 +0700

    updated 2.0

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -7,24 +6,26 @@ import Navbar from './components/Navbar';
 import ManajemenBuku from './components/ManajemenBuku';
 
 function App() {
-  const [books,] = useState([
+  const [books, setBooks] = useState([
     { _id: 1, judul: "Laskar Pelangi", pengarang: "Andrea Hirata", harga: 80000, stok: 7},
-    { _id: 1, judul: "Bumi", pengarang: "Tere Liye", harga: 85000, stok: 5},
-    
+    { _id: 2, judul: "Bumi", pengarang: "Tere Liye", harga: 85000, stok: 5},
   ]); 
+
+  function storeData(inputBook){
+    console.log(inputBook);
+    alert("Data berhasil ditambahkan");
+  }
 
   return (
     <div className="App">
       <BrowserRouter>
-    <Navbar/>
-    
-    <Routes>
-    <Route path="/" exact element={<Beranda/>} />
-    <ManajemenBuku bookList={books} />
-    {/* <Route path="/manajemen-buku" exact element={<ManajemenBuku/>} /> */}
-
-    </Routes>
-    </BrowserRouter>
+        <Navbar/>
+        
+        <Routes>
+          <Route path="/" element={<Beranda />} />
+          <Route path="/manajemen-buku" element={<ManajemenBuku bookList={books} storeData={storeData} editBook={setBooks} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend-react/src/components/ManajemenBuku.jsx
+++ frontend-react/src/components/ManajemenBuku.jsx
@@ -2,8 +2,23 @@ import React, { useState } from 'react';
 import TabelBuku from "./TabelBuku";
 
 
-function ManajemenBuku(bookList)
+function ManajemenBuku({bookList, store})
 {
+    const [inputBook, setInputBook] = useState();
+
+    // ini ada fungsi input tiap label inputan: judul, pengarang, harga dan stok
+    function handleJudul(e) {
+        setInputBook({...inputBook, judul: e.target.value});
+    }
+    function handlePengarang(e) {
+        setInputBook({...inputBook, pengarang: e.target.value});
+    }
+    function handleHarga(e) {
+        setInputBook({...inputBook, harga: e.target.value});
+    }
+    function handleStok(e) {
+        setInputBook({...inputBook, stok: e.target.value});
+    }
     console.log(bookList);
 
     const [formMode, setFormMode] = useState();
@@ -11,8 +26,15 @@ function ManajemenBuku(bookList)
     function showCreateForm(){
         setFormMode("show");
     }
+
     function showEditForm(){
         setFormMode("show");
+    }
+
+    // fungsi button supaya dapat menambahkan
+    function submitAdd(e){
+        e.preventDefault();
+        StorageEvent(inputBook);
     }
 
     return (
@@ -31,6 +53,23 @@ function ManajemenBuku(bookList)
                     <th>Aksi</th>
                 </tr>
             </thead>
+            <tbody>
+            {bookList.map((book, index) => {
+        return (
+            <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{book.judul}</td>
+                <td>{book.pengarang}</td>
+                <td>{book.harga}</td>
+                <td>{book.stok}</td>
+                <td>
+                    <button className="btn btn-sm btn-info mr-3">Edit</button>
+                    <button className="btn btn-sm btn-danger">Hapus</button>
+                </td>
+            </tr>
+        );
+    })}
+</tbody>
         </table>
 
         {/*Input Form*/}
@@ -38,12 +77,18 @@ function ManajemenBuku(bookList)
         <div id="form" className="card py-2 my-3 bg-secondary">
             <div className="card-body">
                 <h4>Hai</h4>
-                <form className="row">
+                <form className="form-row" onSubmit={submitAdd}>
                     <div className="col-6">
-                        <input type="text" name="judul" className="form-control mx-2" placeholder="Judul..." />
+                        <input type="text" name="judul" className="form-control mx-2" onChange={handleJudul} placeholder="Judul..." />
                     </div>
                     <div className="col-4">
-                        <input type="text" name="pengarang" className="form-control mx-2" placeholder="Pengarang..." />
+                        <input type="text" name="pengarang" className="form-control mx-2" onChange={handlePengarang} placeholder="Pengarang..." />
+                    </div>
+                    <div className="col-4">
+                        <input type="text" name="harga" className="form-control mx-2" onChange={handleHarga} placeholder="Harga..." />
+                    </div>
+                    <div className="col-4">
+                        <input type="number" name="stok" className="form-control mx-2" onChange={handleStok} placeholder="Stok..." />
                     </div>
                     <div className="col-2">
                         <input type="submit" className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-green-500" value="submit" />
